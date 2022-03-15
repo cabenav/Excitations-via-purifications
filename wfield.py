56352c8f6d1341058331c2e64c837e5417e6c7ef
Carlos Benavides-Riveros <58853939+cabenav@users.noreply.github.com>
2022-03-15 21:09:13 +0100

    Add files via upload

---
+++ wfield.py
@@ -0,0 +1,85 @@
+import numpy as np
+from numpy import linalg as LA
+import math, cmath
+from scipy.optimize import fmin, minimize, rosen, rosen_der
+from itertools import product
+from copy import copy
+ 
+def expf(x,L):
+   return complex(np.cos(2*np.pi*x/L),np.sin(2*np.pi*x/L))
+
+def Ham(H1,H2,U):
+   return H1+np.multiply(H2,U)
+
+def chop(expr, *, max=0.0000001+0.0000001j):
+    return [i if i > max else 0 for i in expr]
+
+def f(x):
+   return x**2
+
+
+#NUMBER OF SITES:
+L = 8
+
+#GENERATION OF THE HILBERT (FOCK) SPACE
+## This generates the Hilbert space {|000>,|001>,...} but in a non-organized way
+res = [ele for ele in product([0,1], repeat = L)]
+res1 = [ele for ele in product([0,0], repeat = L)]
+res = np.array(res)
+res1 = np.array(res1)
+
+#ORGANIZING THIS IN N-PARTICLE SECTORS
+##res1 contains ALL the states in the Hilbert space
+sumsec= np.sum(res, axis=1)
+vec = []
+for j in range(L+1):
+   vec.append([i for i,x in enumerate(sumsec) if x == j])
+
+order= sum(vec,[])
+for j in range(2**L):
+   res1[j] = res[order[j]]
+
+#GENERATION OF THE ANHILITATION OPERATORS
+##Op[0],Op[1]... are the anhilitation operators for sites 0,1,...
+Op = np.zeros((L,2**L,2**L))
+
+for j in range(L):
+   res2 = [ele for ele in product([0,0], repeat = L)]
+   res2 = copy(res1)
+   res2[:,j] = np.zeros(2**L)
+   aux2 = copy(res1[:,j])
+   if j == 0:
+      sta= copy(res1[:,0])
+   else:
+      sta = [math.pow(-1,value) for value in sum(res1[:,x] for x in range(j))] 
+   for j1 in range(2**L):
+      for j2 in range(2**L):
+         if np.array_equal(res1[j1],res2[j2]):
+            Op[j,j1,j2] = sta[j2]*aux2[j2]
+
+##This optional to verify the fermionic commutation rules:
+#print(np.sum(Op[0],axis=1))
+#print(np.diag(np.matmul(np.transpose(Op[0]), Op[0])+np.matmul(Op[0],np.transpose(Op[0]))))
+
+
+#CONSTRUCTION OF THE HAMILTONIANS
+
+Ham1 =-sum(np.multiply(expf((k1-k2)*jj,L)*expf(-k2,L)/L,np.matmul(np.transpose(Op[k1]), Op[k2]))+np.multiply(expf((k1-k2)*jj,L)*expf(k1,L)/L,np.matmul(np.transpose(Op[k1]), Op[k2])) for k1 in range(L) for k2 in range(L) for jj in range(L))
+
+Ham2 =sum(np.multiply(expf((k1-k2+k3-k4)*jj,L)*expf(k3-k4,L)/L**2,np.matmul(np.matmul(np.matmul(np.transpose(Op[k1]), Op[k2]),np.transpose(Op[k3])),Op[k4])) for k1 in range(L) for k2 in range(L) for k3 in range(L) for k4 in range(L) for jj in range(L))
+
+#EIGENVALUES
+
+print(np.diag(Ham1)[6:15])
+w, v = LA.eig(Ham(Ham1,Ham2,0)[6:15,6:15])
+
+print(w)
+#[L+1:int(L+L*(L-1)/2),L+1:L+int(L*(L-1)/2)])
+
+
+def f(x):
+    return x**2
+
+fmin(f,np.array([0]))
+   
+
