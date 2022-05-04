c3a789b6f5115837ad86c9800b189a64088f8199
Carlos Benavides-Riveros <cabenavir@gmail.com>
2022-05-04 15:40:44 +0200

    Energies_7_2

---
+++ Figures/L=7/grafs.py
@@ -0,0 +1,98 @@
+import numpy as np
+import random
+import pickle
+import gzip as gz
+import csv
+from copy import copy
+import matplotlib.pyplot as plt
+import pickle
+from scipy import interpolate
+from scipy.interpolate import make_lsq_spline, BSpline
+from scipy.interpolate import make_interp_spline
+from scipy.interpolate import Rbf, InterpolatedUnivariateSpline
+from scipy.interpolate import interp1d
+
+nf = 21
+
+list2 =  pickle.load( open( "list1.p", "rb" ) )
+list1 =  pickle.load( open( "list2.p", "rb" ) )
+
+FI1 =[0,1,2,3,4,5, 6, 7, 8, 9,10]
+FI1 = np.array(FI1)
+
+list1[1] = list2[1]
+list1[2] = list2[2]
+
+eigenor= np.zeros((11,nf))
+eigennumor = np.zeros((11,nf))
+gap = np.zeros((11,nf-1))
+gapnum = np.zeros((11,nf-1))
+
+for u in range(11):
+   eigenor[u] = list(list2.real[u])
+   eigenor[u].sort() 
+   eigennumor[u] = list(list1.real[u])
+   eigennumor[u].sort() 
+
+eigennumor[6,14] = eigenor[6,15]
+eigennumor[7,14] = eigenor[7,15]
+eigennumor[8,14] = eigenor[8,15]
+
+for u in range(11):
+   for j in range(5):
+      gap[u,j] = eigenor[u,j+1]-eigenor[u,1]
+      gapnum[u,j] = eigennumor[u][j+1]-eigennumor[u][1]
+   for j in range(5,nf-1):
+      gap[u,j] = eigenor[u,j+1]-eigenor[u,0]
+      gapnum[u,j] = eigennumor[u][j+1]-eigennumor[u][0]
+ 
+
+plt.rc('axes', labelsize=15)
+plt.rc('font', size=15)  
+for i in range(nf-1):
+   plt.plot(FI1, eigenor[:,i],'r-')
+   plt.plot(FI1, eigennumor[:,i],'ko',mfc='none')
+plt.plot(FI1, eigenor[:,nf-1],'r-', mfc='none',label='exact')
+plt.plot(FI1, eigennumor[:,nf-1],'ko', mfc='none',label='UCCSD')
+plt.legend(prop={"size":15},loc='upper left')
+plt.xlabel("$U/t$")
+plt.show()
+
+plt.rc('axes', labelsize=15)
+plt.rc('font', size=15) 
+for i in range(nf-2):
+   plt.plot(FI1, gap[:,i],'r-')
+   plt.plot(FI1, gapnum[:,i],'ko',mfc='none') 
+plt.plot(FI1, gap[:,nf-2],'r-', mfc='none',label='exact')
+plt.plot(FI1, gapnum[:,nf-2],'ko',mfc='none', label='UCCSD')
+plt.legend(prop={"size":15},loc='upper left')
+plt.xlabel("$U/t$")
+plt.show()
+
+def func(vec, vec1):
+   disT=0
+   for j in range(len(vec)):
+      dis = 1
+      for i in range(len(vec1)):
+        # if (float(abs((vec[j].real - vec1[i].real)/vec1[i].real)))**2 < dis:
+         #    dis = (float(abs((vec[j].real - vec1[i].real)/vec1[i].real)))**2
+         if (float(abs((vec[j].real - vec1[i].real)))) < dis:
+             dis = (float(abs((vec[j].real - vec1[i].real))))
+      disT += dis
+   return disT/len(vec1)
+
+error = np.zeros(11)
+errorT = 0
+for i in range(11):
+   error[i] = func(gap[i],gapnum[i])   
+   print(func(gap[i],gapnum[i]))
+   errorT += error[i]
+
+print("Total error: ", errorT/11)
+
+plt.plot(FI1, error,'ko',mfc='none',label='error')
+plt.legend(prop={"size":15},loc='upper left')
+plt.xlabel("$U/t$")
+plt.show()
+
+
