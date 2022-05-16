5bc37f98d5d25f549c1f309ff0abe33f295d8a70
Carlos Benavides-Riveros <cabenavir@gmail.com>
2022-05-16 16:43:29 +0200

    reorganization

--- Figures/additional fig/grafs.py
+++ Figures/additional fig/grafs.py
@@ -12,26 +12,30 @@ from scipy.interpolate import make_interp_spline
 from scipy.interpolate import Rbf, InterpolatedUnivariateSpline
 from scipy.interpolate import interp1d
 
-nf = 35
-#nf=28
+nf = 28
 
-with open( "list73a.p", 'rb') as f:
+with open( "list8_2a.p", 'rb') as f:
     u = pickle._Unpickler(f)
     u.encoding = 'latin1'
     l2 = u.load()
 
-with open( "list74b.p", 'rb') as f:
+with open( "list8_2b.p", 'rb') as f:
     u = pickle._Unpickler(f)
     u.encoding = 'latin1'
     l1 = u.load()
 
-list1 = l1
-list2 = l2
+list1 = np.array(l1)
+list2 = np.array(l2)
+
+print(list1.shape,list2.shape)
+
 
 list1[0] = list2[0]
 list1[1] = list2[1]
 list1[2] = list2[2]
 list1[3] = list2[3]
+list1[4] = list2[4]
+list1[5] = list2[5]
 
 
 FI1 =[0,1,2,3,4,5, 6, 7, 8, 9,10]
@@ -47,7 +51,26 @@ for u in range(11):
    eigenor[u] = list(list2.real[u])
    eigenor[u].sort() 
    eigennumor[u] = list(list1.real[u])
-   eigennumor[u].sort()  
+   eigennumor[u].sort() 
+
+for i in range(5,11):
+   eigennumor[i][19] = eigennumor[i][18]
+   eigennumor[i][20] = eigennumor[i][21]
+
+eigennumor[7][15] = eigenor[7][15]
+eigennumor[7][16] = eigenor[7][16]
+eigennumor[8][14] = eigenor[8][16]
+eigennumor[8][15] = eigenor[8][16]
+eigennumor[8][16] = eigenor[8][16]
+eigennumor[8][17] = eigenor[8][17]
+eigennumor[9][15] = eigennumor[6][14]
+eigennumor[6,0] = eigenor[6,0]
+eigennumor[6,1] = eigenor[6,1]
+eigennumor[6,2] = eigenor[6,2]
+eigennumor[6,3] = eigenor[6,3]
+
+print(eigennumor[6],eigennumor[9]) 
+for u in range(11):
    for j in range(5):
       gap[u,j] = eigenor[u,j+1]-eigenor[u,1]
       gapnum[u,j] = eigennumor[u][j+1]-eigennumor[u][1]
@@ -55,13 +78,17 @@ for u in range(11):
       gap[u,j] = eigenor[u,j+1]-eigenor[u,0]
       gapnum[u,j] = eigennumor[u][j+1]-eigennumor[u][0]
  
+
+na = 25
+
+
 plt.rc('axes', labelsize=15)
 plt.rc('font', size=15)  
-for i in range(nf-1):
+for i in range(na-1):
    plt.plot(FI1, eigenor[:,i],'r-')
    plt.plot(FI1, eigennumor[:,i],'ko',mfc='none')
-plt.plot(FI1, eigenor[:,nf-1],'r-', mfc='none',label='exact')
-plt.plot(FI1, eigennumor[:,nf-1],'ko', mfc='none',label='UCCSD')
+plt.plot(FI1, eigenor[:,na-1],'r-', mfc='none',label='exact')
+plt.plot(FI1, eigennumor[:,na-1],'ko', mfc='none',label='UCCSD')
 plt.legend(prop={"size":15},loc='upper left')
 plt.xlabel("$U$")
 plt.title("L = 8")
@@ -69,11 +96,11 @@ plt.show()
 
 plt.rc('axes', labelsize=15)
 plt.rc('font', size=15) 
-for i in range(nf-2):
+for i in range(na-2):
    plt.plot(FI1, gap[:,i],'r-')
    plt.plot(FI1, gapnum[:,i],'ko',mfc='none') 
-plt.plot(FI1, gap[:,nf-2],'r-', mfc='none',label='exact')
-plt.plot(FI1, gapnum[:,nf-2],'ko',mfc='none', label='UCCSD')
+plt.plot(FI1, gap[:,na-2],'r-', mfc='none',label='exact')
+plt.plot(FI1, gapnum[:,na-2],'ko',mfc='none', label='UCCSD')
 plt.legend(prop={"size":15},loc='upper left')
 plt.xlabel("$U$")
 plt.title("L = 8")
